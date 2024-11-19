8fb61a5eed5960be75646d6233929a9265dde49d
Manish kumar <134037752+Manish-kumar-9601@users.noreply.github.com>
2024-11-19 19:07:53 +0530

    change
    Signed-off-by: Manish kumar <134037752+Manish-kumar-9601@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
       <img src="./src/assets/profile.jpg" alt="profile" className='h-36 aspect-square object-cover rounded-full m-7 border-l-2 border-blue-400 border-r-2 ' />
       
-      <div className="max-w-sm w-full bg-[url('./src/assets/winter.jpg')] bg-cover rounded-xl shadow-2xl p-8">
+      <div className="max-w-sm w-full bg-[url('chnage./src/assets/winter.jpg')] bg-cover rounded-xl shadow-2xl p-8">
         <div className="text-center mb-8">
           <h2 className="text-3xl font-bold text-gray-900 mb-2">Manish Kumar</h2>
           <p className="text-gray-800">Full Stack Developer</p>
